2922b694d3e7b8b9702213b83a94fef0d9b04a3c
XHEKI <pashokts@mail.ru>
2025-07-23 22:26:29 +0300

    Added 2 new crud functions and 2 new paths. Also added new schemas.

--- app/api/user_endpoints.py
+++ app/api/user_endpoints.py
@@ -1,6 +1,6 @@
 import crud.crud as crud_operations
 from fastapi import Depends, APIRouter
-from schemas.schemas_recipe import CreateUser, User
+from schemas.schemas_recipe import CreateUser, User, UpdateUser
 from sqlmodel import Session
 from db.db import get_session
 from uuid import UUID
@@ -15,3 +15,11 @@ def create_user(user: CreateUser, session: Session = Depends(get_session)) -> Us
 @user_router.delete("/delete_user/{user_id}")
 def delete_user(user_id: UUID, session: Session = Depends(get_session)) -> str:
     return crud_operations.delete_user(user_id, session)
+
+@user_router.get("/get_users")
+def get_users(session: Session = Depends(get_session)) -> list[User]:
+    return crud_operations.get_users(session)
+
+@user_router.patch("/update_user_data/{user_id}")
+def update_user(user_id: UUID, data: UpdateUser, session: Session = Depends(get_session)) -> User:
+    return crud_operations.update_user_data(user_id, data, session)
--- app/schemas/schemas_recipe.py
+++ app/schemas/schemas_recipe.py
@@ -1,5 +1,6 @@
-from pydantic import EmailStr, BaseModel, field_validator
+from pydantic import EmailStr, BaseModel, field_validator, model_validator
 from uuid import UUID
+from typing import Optional
 
 class CreateUser(BaseModel):
     username: str
@@ -9,6 +10,29 @@ class CreateUser(BaseModel):
     @field_validator("username")
     def capitalize(cls, value):
         return value.title()
+    
+    @field_validator("password")
+    def length_check(cls, value):
+        if len(value) <= 7:
+            raise ValueError("the password must be at least 8 symbols long.")
+        
+        return value
 
 class User(CreateUser):
     id: UUID
+
+
+class UpdateUser(BaseModel):
+    username: Optional[str] | None = None
+    email: Optional[EmailStr] | None = None
+    password: Optional[str] | None = None
+
+    @model_validator(mode="after")
+    def validate_update(self):
+        if not self.username and not self.email and not self.password:
+            raise ValueError("data was not provided.")
+
+        if self.email and not self.password:
+            raise ValueError("when changing email, password must be provided.")
+        
+        return self
